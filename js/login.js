6c5e33659a269b1bc92f560581c4a30c648f0a44
Faisal Chakiki <faisalchakiki012018@gmail.com>
2022-11-16 10:33:44 +0700

    dinamis page form and fix js

--- js/login.js
+++ js/login.js
@@ -1,36 +1,72 @@
+let a = document.location.href
+console.log(a)
 // Show Password
-function showPass(password) {
+function showPass(password, eye) {
+  const eyes = document.getElementById(eye);
+  console.log(eye)
   var pass = document.getElementById(password);
   if (pass.type === "password") {
     pass.type = "text";
+    eyes.classList.add("active-eye");
   } else {
     pass.type = "password";
+    eyes.classList.remove("active-eye");
   }
 }
 
-// Validation login
-let alertEmail = document.querySelector(".val-email")
-let alertPass = document.querySelector(".val-pass")
-let login = document.getElementById("login-form")
-login.addEventListener("submit", (event)=>{
-  event.preventDefault()
-  let valueEmail = event.target.email.value
-  let valuePass = event.target.password.value
-  if(valueEmail == "root@admin.com" && valuePass == "rahasia"){
-    document.location.href = '../page/manage-movie.html'
-  }else if(valueEmail != "faisal@gmail.com"){
-    alertEmail.classList.remove("hidden")
-  }else if(valuePass != "123"){
-    alertPass.classList.remove("hidden")
-  }else{
-    alertEmail.classList.add("hidden")
-    alertPass.classList.add("hidden")
-    document.location.href ='../page/home-withuser.html';
-  }
-})
-document.getElementById("email").addEventListener("click",()=>{
-  alertEmail.classList.add("hidden")
-})
-document.getElementById("password").addEventListener("click",()=>{
-  alertPass.classList.add("hidden")
-})
+if (document.location.pathname == "/page/sign-in.html") {
+  // Validation login
+  let alertEmail = document.querySelector(".val-email");
+  let alertPass = document.querySelector(".val-pass");
+  let login = document.getElementById("login-form");
+  login.addEventListener("submit", (event) => {
+    event.preventDefault();
+    let valueEmail = event.target.email.value;
+    let valuePass = event.target.password.value;
+    if (valueEmail == "root@admin.com" && valuePass == "rahasia") {
+      document.location.href = "../page/manage-movie.html";
+    } else if (valueEmail != "faisal@gmail.com") {
+      alertEmail.classList.remove("hidden");
+    } else if (valuePass != "123") {
+      alertPass.classList.remove("hidden");
+    } else {
+      document.location.href = "../page/home-withuser.html";
+    }
+  });
+  document.getElementById("email").addEventListener("click", () => {
+    alertEmail.classList.add("hidden");
+  });
+  document.getElementById("password").addEventListener("click", () => {
+    alertPass.classList.add("hidden");
+  });
+}else if(document.location.pathname == "/page/sign-up.html"){
+  const register = document.getElementById("register-form");
+  register.addEventListener("submit", (event)=>{
+    event.preventDefault()
+    document.location.href = "./home-withuser.html"
+  })
+}else if(document.location.pathname == "/page/reset1.html"){
+  const checkEmail = document.getElementById("forgot-email");
+  checkEmail.addEventListener("submit", (event)=>{
+    event.preventDefault()
+    document.location.href = "./reset2.html"
+  })
+}else if(document.location.pathname == "/page/reset2.html"){
+  const setPassword = document.getElementById("set-password");
+  let alertPass = document.querySelector(".val-pass");
+  setPassword.addEventListener("submit", (event)=>{
+    event.preventDefault()
+    const pass1 = event.target.password.value;
+    console.log(pass1)
+    const pass2 = event.target.password2.value;
+    console.log(pass2)
+    if(pass1 === pass2){
+      document.location.href = "./home-withuser.html"
+    }else{
+      alertPass.classList.remove("hidden")
+    }
+    document.getElementById("password2").addEventListener("click", () => {
+      alertPass.classList.add("hidden");
+    });
+  })
+}
--- js/script.js
+++ js/script.js
@@ -1,222 +1,199 @@
-//Database Movie
-const dataMovie = [
-  {
-    title: "Spiderman-Man Homecoming",
-    genre: "Action, Adventure, Sci-FI",
-    image: "../asset/img/movie.png",
-    dateRelease: "",
-    author: "",
-    casts: "",
-    duration: "",
-    synopsis: ""
-  },
-  {
-    title: "Spiderman-Man No Way Home",
-    genre: "Action, Adventure, Sci-FI",
-    image:
-      "https://www.themoviedb.org/t/p/w300_and_h450_bestv2/uJYYizSuA9Y3DCs0qS4qWvHfZg4.jpg",
-    dateRelease: "",
-    author: "",
-    casts: "",
-    duration: "",
-    synopsis: ""
+// let a = document.location.pathname
+// console.log(a)
+if(document.location.pathname == "/index.html" || document.location.pathname == "/page/home-withuser.html" ){
+  //Database Movie
+  const dataMovie = [
+    {
+      title: "Spiderman-Man Homecoming",
+      genre: "Action, Adventure, Sci-FI",
+      image: "../asset/img/movie.png",
+      dateRelease: "",
+      author: "",
+      casts: "",
+      duration: "",
+      synopsis: ""
     },
-  {
-    title: "Peaky Blinders",
-    genre: "Drama, Crime",
-    image:
-      "https://www.themoviedb.org/t/p/w300_and_h450_bestv2/vUUqzWa2LnHIVqkaKVlVGkVcZIW.jpg",
-    dateRelease: "",
-    author: "",
-    casts: "",
-    duration: "",
-    synopsis: ""
-    },
-  {
-    title: "The Walking Dead",
-    genre: "Action, Adventure, Drama, Fantasy",
-    image:
-      "https://www.themoviedb.org/t/p/w300_and_h450_bestv2/xf9wuDcqlUPWABZNeDKPbZUjWx0.jpg",
-    dateRelease: "",
-    author: "",
-    casts: "",
-    duration: "",
-    synopsis: ""
-    },
-  {
-    title: "The Takeover",
-    genre: "Action, Crime, Thriller",
-    image:
-      "https://www.themoviedb.org/t/p/w300_and_h450_bestv2/g7rdcofib7HqdlDP1LT7Hmf1f2o.jpg",
-    dateRelease: "",
-    author: "",
-    casts: "",
-    duration: "",
-    synopsis: ""
-    },
-  {
-    title: "Manifest",
-    genre: "Drama, Mystery, Sci-FI",
-    image:
-      "https://www.themoviedb.org/t/p/w300_and_h450_bestv2/1xeiUxShzNn8TNdMqy3Hvo9o2R.jpg",
-    dateRelease: "",
-    author: "",
-    casts: "",
-    duration: "",
-    synopsis: ""
-    },
-];
+    {
+      title: "Spiderman-Man No Way Home",
+      genre: "Action, Adventure, Sci-FI",
+      image:
+        "https://www.themoviedb.org/t/p/w300_and_h450_bestv2/uJYYizSuA9Y3DCs0qS4qWvHfZg4.jpg",
+      dateRelease: "",
+      author: "",
+      casts: "",
+      duration: "",
+      synopsis: ""
+      },
+    {
+      title: "Peaky Blinders",
+      genre: "Drama, Crime",
+      image:
+        "https://www.themoviedb.org/t/p/w300_and_h450_bestv2/vUUqzWa2LnHIVqkaKVlVGkVcZIW.jpg",
+      dateRelease: "",
+      author: "",
+      casts: "",
+      duration: "",
+      synopsis: ""
+      },
+    {
+      title: "The Walking Dead",
+      genre: "Action, Adventure, Drama, Fantasy",
+      image:
+        "https://www.themoviedb.org/t/p/w300_and_h450_bestv2/xf9wuDcqlUPWABZNeDKPbZUjWx0.jpg",
+      dateRelease: "",
+      author: "",
+      casts: "",
+      duration: "",
+      synopsis: ""
+      },
+    {
+      title: "The Takeover",
+      genre: "Action, Crime, Thriller",
+      image:
+        "https://www.themoviedb.org/t/p/w300_and_h450_bestv2/g7rdcofib7HqdlDP1LT7Hmf1f2o.jpg",
+      dateRelease: "",
+      author: "",
+      casts: "",
+      duration: "",
+      synopsis: ""
+      },
+    {
+      title: "Manifest",
+      genre: "Drama, Mystery, Sci-FI",
+      image:
+        "https://www.themoviedb.org/t/p/w300_and_h450_bestv2/1xeiUxShzNn8TNdMqy3Hvo9o2R.jpg",
+      dateRelease: "",
+      author: "",
+      casts: "",
+      duration: "",
+      synopsis: ""
+      },
+  ];
+  
+  const dataMovieUC = [
+    {
+      title: "Manifest",
+      genre: "Drama, Mystery, Sci-FI",
+      image:
+        "https://www.themoviedb.org/t/p/w300_and_h450_bestv2/1xeiUxShzNn8TNdMqy3Hvo9o2R.jpg",
+      dateRelease: "",
+      author: "",
+      casts: "",
+      duration: "",
+      synopsis: ""
+      },
+    {
+      title: "Wild is the Wind",
+      genre: "Crime, Drama",
+      image:
+        "https://www.themoviedb.org/t/p/w300_and_h450_bestv2/rITxQBtnMpneZf8QzH1dqONQocx.jpg",
+      dateRelease: "",
+      author: "",
+      casts: "",
+      duration: "",
+      synopsis: ""
+      },
+    {
+      title: "Starwars: Andor",
+      genre: "Fantasy, War&Politics, Sci-FI",
+      image:
+        "https://www.themoviedb.org/t/p/w300_and_h450_bestv2/59SVNwLfoMnZPPB6ukW6dlPxAdI.jpg",
+      dateRelease: "",
+      author: "",
+      casts: "",
+      duration: "",
+      synopsis: ""
+      },
+    {
+      title: "Chainsaw Man",
+      genre: "Animation, Action, Sci-FI",
+      image:
+        "https://www.themoviedb.org/t/p/w300_and_h450_bestv2/yVtx7Xn9UxNJqvG2BkvhCcmed9S.jpg",
+      dateRelease: "",
+      author: "",
+      casts: "",
+      duration: "",
+      synopsis: ""
+      },
+    {
+      title: "Overdose",
+      genre: "Action, Crime, Thiller",
+      image:
+        "https://www.themoviedb.org/t/p/w300_and_h450_bestv2/9RvM4wawLRlX608FgZr9kL8CLmy.jpg",
+      dateRelease: "",
+      author: "",
+      casts: "",
+      duration: "",
+      synopsis: ""
+      },
+  ];
+  
+  // Movie
+  const wrapperMovie = document.getElementById("content-movie");
+  dataMovie.forEach((data) => {
+    const boxMovie = document.createElement("div");
+    boxMovie.className = "box-movie box-active";
+    const picture = document.createElement("img");
+    picture.src = data.image;
+    picture.alt = "image-movie";
+    picture.loading = "lazy";
+    const textMovie = document.createElement("div");
+    textMovie.className = "text-movie hidden";
+    const titleMovie = document.createElement("h4");
+    titleMovie.className = "title-movie";
+    titleMovie.textContent = data.title;
+    const genre = document.createElement("p");
+    genre.className = "genre";
+    genre.textContent = data.genre;
+    const buttonDetail = document.createElement("button");
+    buttonDetail.className = "details";
+    const details = document.createElement("a");
+    details.href = document.location == 'index.html' ? "./page/details-noUser.html" : "../page/details.html";
+    details.textContent = "Details";
+  
+    wrapperMovie.appendChild(boxMovie);
+    boxMovie.appendChild(picture);
+    textMovie.appendChild(titleMovie);
+    textMovie.appendChild(genre);
+    buttonDetail.appendChild(details);
+    textMovie.appendChild(buttonDetail);
+    boxMovie.appendChild(textMovie);
+  });
+  
+  // UpComing Movie
+  const wrapperUCMovie = document.getElementById("uc-movies");
+  dataMovieUC.forEach((data) => {
+    const cardMovie = document.createElement("div");
+    cardMovie.className = "card-movie";
+    const picture = document.createElement("img");
+    picture.src = data.image;
+    picture.alt = "image-movie";
+    picture.loading = "lazy";
+    const textMovie = document.createElement("div");
+    textMovie.className = "text-movie";
+    const titleMovie = document.createElement("h4");
+    titleMovie.className = "title-movie";
+    titleMovie.textContent = data.title;
+    const genre = document.createElement("p");
+    genre.className = "genre";
+    genre.textContent = data.genre;
+    const buttonDetail = document.createElement("button");
+    buttonDetail.className = "details";
+    const details = document.createElement("a");
+    details.href = document.location == 'index.html' ? "./page/details-noUser.html" : "../page/details.html";
+    details.textContent = "Details";
+  
+    wrapperUCMovie.appendChild(cardMovie);
+    cardMovie.appendChild(picture);
+    textMovie.appendChild(titleMovie);
+    textMovie.appendChild(genre);
+    buttonDetail.appendChild(details);
+    textMovie.appendChild(buttonDetail);
+    cardMovie.appendChild(textMovie);
+  });
+}else if(document.location.pathname == "/page/details.html"){
+// Set New Date
+const date = document.getElementById("date") 
+const newDate = date.value = new Date().toISOString().slice(0, 10);
+}
 
-const dataMovieUC = [
-  {
-    title: "Manifest",
-    genre: "Drama, Mystery, Sci-FI",
-    image:
-      "https://www.themoviedb.org/t/p/w300_and_h450_bestv2/1xeiUxShzNn8TNdMqy3Hvo9o2R.jpg",
-    dateRelease: "",
-    author: "",
-    casts: "",
-    duration: "",
-    synopsis: ""
-    },
-  {
-    title: "Wild is the Wind",
-    genre: "Crime, Drama",
-    image:
-      "https://www.themoviedb.org/t/p/w300_and_h450_bestv2/rITxQBtnMpneZf8QzH1dqONQocx.jpg",
-    dateRelease: "",
-    author: "",
-    casts: "",
-    duration: "",
-    synopsis: ""
-    },
-  {
-    title: "Starwars: Andor",
-    genre: "Fantasy, War&Politics, Sci-FI",
-    image:
-      "https://www.themoviedb.org/t/p/w300_and_h450_bestv2/59SVNwLfoMnZPPB6ukW6dlPxAdI.jpg",
-    dateRelease: "",
-    author: "",
-    casts: "",
-    duration: "",
-    synopsis: ""
-    },
-  {
-    title: "Chainsaw Man",
-    genre: "Animation, Action, Sci-FI",
-    image:
-      "https://www.themoviedb.org/t/p/w300_and_h450_bestv2/yVtx7Xn9UxNJqvG2BkvhCcmed9S.jpg",
-    dateRelease: "",
-    author: "",
-    casts: "",
-    duration: "",
-    synopsis: ""
-    },
-  {
-    title: "Overdose",
-    genre: "Action, Crime, Thiller",
-    image:
-      "https://www.themoviedb.org/t/p/w300_and_h450_bestv2/9RvM4wawLRlX608FgZr9kL8CLmy.jpg",
-    dateRelease: "",
-    author: "",
-    casts: "",
-    duration: "",
-    synopsis: ""
-    },
-];
-
-// Movie
-const wrapperMovie = document.getElementById("content-movie");
-dataMovie.forEach((data) => {
-  const boxMovie = document.createElement("div");
-  boxMovie.className = "box-movie box-active";
-  const picture = document.createElement("img");
-  picture.src = data.image;
-  picture.alt = "image-movie";
-  picture.loading = "lazy";
-  const textMovie = document.createElement("div");
-  textMovie.className = "text-movie hidden";
-  const titleMovie = document.createElement("h4");
-  titleMovie.className = "title-movie";
-  titleMovie.textContent = data.title;
-  const genre = document.createElement("p");
-  genre.className = "genre";
-  genre.textContent = data.genre;
-  const buttonDetail = document.createElement("button");
-  buttonDetail.className = "details";
-  const details = document.createElement("a");
-  details.href = "./page/details-noUser.html";
-  details.textContent = "Details";
-
-  wrapperMovie.appendChild(boxMovie);
-  boxMovie.appendChild(picture);
-  textMovie.appendChild(titleMovie);
-  textMovie.appendChild(genre);
-  buttonDetail.appendChild(details);
-  textMovie.appendChild(buttonDetail);
-  boxMovie.appendChild(textMovie);
-});
-
-// UpComing Movie
-const wrapperUCMovie = document.getElementById("uc-movies");
-dataMovieUC.forEach((data) => {
-  const cardMovie = document.createElement("div");
-  cardMovie.className = "card-movie";
-  const picture = document.createElement("img");
-  picture.src = data.image;
-  picture.alt = "image-movie";
-  picture.loading = "lazy";
-  const textMovie = document.createElement("div");
-  textMovie.className = "text-movie";
-  const titleMovie = document.createElement("h4");
-  titleMovie.className = "title-movie";
-  titleMovie.textContent = data.title;
-  const genre = document.createElement("p");
-  genre.className = "genre";
-  genre.textContent = data.genre;
-  const buttonDetail = document.createElement("button");
-  buttonDetail.className = "details";
-  const details = document.createElement("a");
-  details.href = "./page/details-noUser.html";
-  details.textContent = "Details";
-
-  wrapperUCMovie.appendChild(cardMovie);
-  cardMovie.appendChild(picture);
-  textMovie.appendChild(titleMovie);
-  textMovie.appendChild(genre);
-  buttonDetail.appendChild(details);
-  textMovie.appendChild(buttonDetail);
-  cardMovie.appendChild(textMovie);
-});
-
-const wrapMovie = document.getElementById("movie-content");
-console.log
-dataMovie.forEach((data) => {
-  const boxMovie = document.createElement("div");
-  boxMovie.className = "box-movie box-active";
-  const picture = document.createElement("img");
-  picture.src = data.image;
-  picture.alt = "image-movie";
-  picture.loading = "lazy";
-  const textMovie = document.createElement("div");
-  textMovie.className = "text-movie hidden";
-  const titleMovie = document.createElement("h4");
-  titleMovie.className = "title-movie";
-  titleMovie.textContent = data.title;
-  const genre = document.createElement("p");
-  genre.className = "genre";
-  genre.textContent = data.genre;
-  const buttonDetail = document.createElement("button");
-  buttonDetail.className = "details";
-  const details = document.createElement("a");
-  details.href = "./page/details-noUser.html";
-  details.textContent = "Details";
-
-  wrapMovie.appendChild(boxMovie);
-  boxMovie.appendChild(picture);
-  textMovie.appendChild(titleMovie);
-  textMovie.appendChild(genre);
-  buttonDetail.appendChild(details);
-  textMovie.appendChild(buttonDetail);
-  boxMovie.appendChild(textMovie);
-});
